5fd13738299c0f99bed88c446d84b0d594a78b4c
deciraptor <dethevenaux@gmail.com>
2025-05-12 17:04:19 +0200

    Mise en place de la page minimum. Ajout de bootstrap et bootstrap-icons. Ajout de normalize et de jquery.

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#menuToggle').click(function(){
+        $('#mainNav').stop(true, true).slideToggle()
+    })
+})
+
+$(document).ready(function(){
+    $('#menuFrog').click(function(){
+        $('#footerNav').stop(true, true).slideToggle()
+    })
+})
