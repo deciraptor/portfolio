9bdc9021d114994bad21bf1917fe55836ec5d44f
deciraptor <dethevenaux@gmail.com>
2025-05-14 21:25:57 +0200

    ajout du changement de style du header lors du scroll down

---
+++ js/main.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("mainHeader").style.backgroundColor = "#191919";
+    document.getElementById("mainHeader").style.borderBottom = "4px solid #1d1c2a";
+
+  } else {
+    document.getElementById("mainHeader").style.backgroundColor = "transparent";
+    document.getElementById("mainHeader").style.borderBottom = "none";
+
+  }
+}
