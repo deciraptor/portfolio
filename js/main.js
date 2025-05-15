96633b52fc9bf3ae251791e34a093b95322d8d77
deciraptor <dethevenaux@gmail.com>
2025-05-15 12:01:19 +0200

    amélioration responsivité section aboutMe et section skills

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("mainHeader").style.backgroundColor = "#191919";
-    document.getElementById("mainHeader").style.borderBottom = "1px solid #19c79a";
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("mainHeader").style.backgroundColor = "#191919";
+        document.getElementById("mainHeader").style.borderBottom = "1px solid #19c79a";
 
-  } else {
-    document.getElementById("mainHeader").style.backgroundColor = "transparent";
-    document.getElementById("mainHeader").style.borderBottom = "none";
+    } else {
+        document.getElementById("mainHeader").style.backgroundColor = "transparent";
+        document.getElementById("mainHeader").style.borderBottom = "none";
 
-  }
+    }
 }
+
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(el => {
+    new bootstrap.Tooltip(el);
+});
